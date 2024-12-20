44dbb2aa618629a25b6311608f4258b6628615b2
Joni Korpi <me@jonikorpi.com>
2024-12-20 22:05:22 +0200

    Remove filter Q tricks

--- instrumentPresets.js
+++ instrumentPresets.js
@@ -131,8 +131,6 @@ export const flute = {
   lowPassFrequency: 2349.312,
 
   lowPassPitchTracking: 0.618,
-  lowPassQ: 2.0,
-  highPassQ: 2.0,
 
   vibratoEffectOnLowPass: 500.0,
   peakingFilters: [{ frequency: 810, gain: 2.0, Q: 2.0 }],
@@ -598,9 +596,6 @@ export const violin = {
   highPassFrequency: 196.0,
   lowPassFrequency: 3520.0,
 
-  lowPassQ: 2.0,
-  highPassQ: 2.0,
-
   vibratoEffectOnPitch: 30,
   peakingFilters: [
     { frequency: 300, gain: 3, Q: 3.5 },
@@ -823,9 +818,6 @@ export const piano = {
   sustain: 0.0,
   release: 0.0,
 
-  highPassQ: 2.0,
-  lowPassQ: 2.0,
-
   highPassPitchTracking: -2.5, // strings don't really emit fundamentals under 100 hz
   lowPassPitchTracking: 2.0,
 };
@@ -882,9 +874,6 @@ export const hammeredDulcimer = {
   sustain: 0.0,
   release: 0.0,
 
-  highPassQ: 2.0,
-  lowPassQ: 2.0,
-
   highPassFrequency: 73.42,
   lowPassFrequency: 1244.51 * 2.0,
   highPassPitchTracking: -0.333333, // strings don't really emit fundamentals under 100 hz
@@ -936,9 +925,6 @@ export const taikoDrum = {
   filterDecay: 0.618,
   sustain: 0.0,
   release: 0.0,
-
-  highPassQ: 2.0,
-  lowPassQ: 2.0,
 
   highPassFrequency: 44.0,
   lowPassFrequency: 2200.0,
@@ -1117,7 +1103,7 @@ export const glockenspiel = {
   ],
 
   decay: marimba.decay,
-  filterDecay: marimba.decay * 0.618
+  filterDecay: marimba.decay * 0.618,
 };
 
 // Plucked versions of string instruments
@@ -1139,9 +1125,6 @@ const makePlucked = (instrument) => {
     filterSustain: 0.0,
     release: 0.0,
     filterRelease: 0.0,
-
-    highPassQ: 2.618,
-    lowPassQ: 2.618,
 
     vibratoEffectOnPitch: 20.0,
     vibratoEffectOnVolume: 0.0,
