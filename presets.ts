8288bddfad3ecf32502dc007c1b7f086fdb1d7a2
Joni Korpi <me@jonikorpi.com>
2025-08-03 15:43:36 +0300

    Add brass presets

--- presets.ts
+++ presets.ts
@@ -1,23 +1,24 @@
 import type { InstrumentPreset } from "./Instrument.ts";
 import { midiToFrequency } from "./notes.js";
 
+const fluteEnvelope = { attack: 0.056, decay: 0.382, release: 0.124 };
+const reedEnvelope = { ...fluteEnvelope, attack: 0.09 };
+// TODO: initial pitch instability
+const brassEnvelope = { ...reedEnvelope, release: 0.146 };
+
 // https://northwoodsoboe.com/the-oboes-overtones-why-does-the-oboe-sound-so-unique/
 // https://musiccrashcourses.com/lessons/harmonic_series.html
 // https://www.youtube.com/watch?v=hfS7mDvrZ7g
 export const flute: InstrumentPreset = {
   partials: [[1.0], [0.382], [0.618], [0.146], [0.236], [0.056], [0.09], [0.013], [0.034], [0.005], [0.013], [0.002]],
-  attack: 0.034,
-  decay: 0.382,
-  release: 0.146,
-  inharmonicity: 0.005,
+  ...fluteEnvelope,
+  inharmonicity: 0.003,
   formantFrequency: midiToFrequency(69),
 };
 
 // https://people.ece.cornell.edu/land/courses/ece5760/FinalProjects/f2011/emr76_jmm536/emr76_jmm536/index.html
 // https://www.physicsforums.com/threads/origin-of-harmonics-in-helmholts-type-resonators.799974/
 export const ocarina: InstrumentPreset = {
-  ...flute,
-  inharmonicity: 0.008,
   partials: [
     [1.0, 1],
     [0.034, 3],
@@ -25,15 +26,15 @@ export const ocarina: InstrumentPreset = {
     [0.013, 7],
     [0.008, 9],
   ],
+  ...fluteEnvelope,
 };
 
 // https://northwoodsoboe.com/the-oboes-overtones-why-does-the-oboe-sound-so-unique/
 // https://www.youtube.com/watch?v=a0-ysmiQTss
 export const oboe: InstrumentPreset = {
-  ...flute,
   partials: [[0.618], [0.382], [0.764], [1.0], [0.618], [0.236], [0.382], [0.146], [0.056], [0.021], [0.008], [0.003]],
-  attack: 0.056,
-  inharmonicity: 0.013,
+  ...reedEnvelope,
+  inharmonicity: 0.008,
   formantFrequency: midiToFrequency(65),
 };
 
@@ -64,7 +65,6 @@ export const bassoon: InstrumentPreset = {
 // https://newt.phys.unsw.edu.au/jw/inharmonic-resonances.html
 // https://www.youtube.com/watch?v=lhvJUU9Js-U
 export const clarinet: InstrumentPreset = {
-  ...oboe,
   partials: [
     [1.0],
     [0.056], // evens are very weak
@@ -81,6 +81,7 @@ export const clarinet: InstrumentPreset = {
     [0.034],
     [0.002],
   ],
+  ...reedEnvelope,
   inharmonicity: 0.018,
   formantFrequency: midiToFrequency(62),
 };
@@ -93,9 +94,8 @@ export const clarinet: InstrumentPreset = {
 // https://www.physics.rutgers.edu/~jackph/2005s/sm_fft/sm_fft.html
 // https://newt.phys.unsw.edu.au/jw/inharmonic-resonances.html
 export const saxophone: InstrumentPreset = {
-  ...clarinet,
   partials: [
-    [0.764, undefined, 1.0 / 9.0],
+    [0.764, undefined, 1.0 / 10.0],
     [1.0, undefined, 1.0 / 11.0],
     [0.618, undefined, 1.0 / 9.0],
     [0.09, undefined, 1.0 / 10.0],
@@ -109,9 +109,91 @@ export const saxophone: InstrumentPreset = {
     [0.056, undefined, 1.0 / 2.0],
     [0.034, undefined, 1.0],
   ],
-  // TODO: initial pitch instability
+  ...reedEnvelope,
   inharmonicity: 0.018,
   formantFrequency: midiToFrequency(64),
+};
+
+// https://northwoodsoboe.com/the-oboes-overtones-why-does-the-oboe-sound-so-unique/
+// https://www.youtube.com/watch?v=2f5TxqlLUEs
+export const trumpet: InstrumentPreset = {
+  partials: [
+    [0.764, undefined, 1.0 / 11.0],
+    [1.0, undefined, 1.0 / 12.0],
+    [0.764, undefined, 1.0 / 10.0],
+    [0.382, undefined, 1.0 / 9.0],
+    [0.382, undefined, 1.0 / 8.0],
+    [0.146, undefined, 1.0 / 7.0],
+    [0.09, undefined, 1.0 / 6.0],
+    [0.056, undefined, 1.0 / 5.0],
+    [0.056, undefined, 1.0 / 4.0],
+    [0.021, undefined, 1.0 / 3.0],
+    [0.013, undefined, 1.0 / 2.0],
+    [0.008, undefined, 1.0],
+  ],
+  ...brassEnvelope,
+  formantFrequency: midiToFrequency(67),
+};
+
+// https://www.researchgate.net/figure/Power-spectrum-of-flute-trombone-and-their-mixture_fig3_226825024
+// http://hyperphysics.phy-astr.gsu.edu/hbase/Music/tromw.html
+// https://www.youtube.com/watch?v=2f5TxqlLUEs
+export const trombone: InstrumentPreset = {
+  partials: [
+    [0.764, undefined, 1.0 / 10.0],
+    [1.0, undefined, 1.0 / 9.0],
+    [0.854, undefined, 1.0 / 8.0],
+    [0.618, undefined, 1.0 / 7.0],
+    [0.236, undefined, 1.0 / 6.0],
+    [0.09, undefined, 1.0 / 5.0],
+    [0.034, undefined, 1.0 / 4.0],
+    [0.013, undefined, 1.0 / 3.0],
+    [0.005, undefined, 1.0 / 2.0],
+    [0.002, undefined, 1.0 / 1.0],
+  ],
+  ...brassEnvelope,
+  formantFrequency: midiToFrequency(60),
+};
+
+// https://www.researchgate.net/figure/Spectrum-comparison-of-different-instrument-objects-On-the-left-hand-side-C-Trumpet-C_fig7_225163040
+// https://www.youtube.com/watch?v=2f5TxqlLUEs
+export const frenchHorn: InstrumentPreset = {
+  partials: [
+    [1.0, undefined, 1.0 / 12.0],
+    [0.618, undefined, 1.0 / 11.0],
+    [0.382, undefined, 1.0 / 10.0],
+    [0.236, undefined, 1.0 / 9.0],
+    [0.146, undefined, 1.0 / 8.0],
+    [0.09, undefined, 1.0 / 7.0],
+    [0.056, undefined, 1.0 / 6.0],
+    [0.034, undefined, 1.0 / 5.0],
+    [0.021, undefined, 1.0 / 4.0],
+    [0.013, undefined, 1.0 / 3.0],
+    [0.008, undefined, 1.0 / 2.0],
+    [0.005, undefined, 1.0],
+  ],
+  ...brassEnvelope,
+  formantFrequency: midiToFrequency(65),
+};
+
+// https://www.rickdenney.com/the_tuba_sound.htm
+export const tuba: InstrumentPreset = {
+  partials: [
+    [0.764, undefined, 1.0 / 12.0],
+    [0.854, undefined, 1.0 / 11.0],
+    [0.764, undefined, 1.0 / 10.0],
+    [1.0, undefined, 1.0 / 9.0],
+    [0.382, undefined, 1.0 / 8.0],
+    [0.618, undefined, 1.0 / 7.0],
+    [0.382, undefined, 1.0 / 6.0],
+    [0.146, undefined, 1.0 / 5.0],
+    [0.056, undefined, 1.0 / 4.0],
+    [0.021, undefined, 1.0 / 3.0],
+    [0.008, undefined, 1.0 / 2.0],
+    [0.003, undefined, 1.0],
+  ],
+  ...brassEnvelope,
+  formantFrequency: midiToFrequency(65),
 };
 
 export const cello: InstrumentPreset = {
